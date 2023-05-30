fcb18832c1e7b6085d9b1979366b431854590169
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2023-05-29 21:42:03 -0300

    up

--- build/CMakeFiles/TDC.dir/src/Myhill-Nerode/minimization.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/TDC.dir/src/Myhill-Nerode/minimization.c.o: \
- /home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Myhill-Nerode/minimization.c \
- /usr/include/stdc-predef.h \
- /home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Myhill-Nerode/minimization.h
