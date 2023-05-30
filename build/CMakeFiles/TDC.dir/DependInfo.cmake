129688d8a02266ef02680b1ee16a0a9da1f12db5
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2023-05-29 21:33:34 -0300

    configurei o meu CMake

---
+++ build/CMakeFiles/TDC.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Myhill-Nerode/minimization.c" "CMakeFiles/TDC.dir/src/Myhill-Nerode/minimization.c.o" "gcc" "CMakeFiles/TDC.dir/src/Myhill-Nerode/minimization.c.o.d"
+  "/home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Threading/threading.c" "CMakeFiles/TDC.dir/src/Threading/threading.c.o" "gcc" "CMakeFiles/TDC.dir/src/Threading/threading.c.o.d"
+  "/home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Threading/wait_group.c" "CMakeFiles/TDC.dir/src/Threading/wait_group.c.o" "gcc" "CMakeFiles/TDC.dir/src/Threading/wait_group.c.o.d"
+  "/home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/dictionary/dictionary.c" "CMakeFiles/TDC.dir/src/dictionary/dictionary.c.o" "gcc" "CMakeFiles/TDC.dir/src/dictionary/dictionary.c.o.d"
+  "/home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/input/input.c" "CMakeFiles/TDC.dir/src/input/input.c.o" "gcc" "CMakeFiles/TDC.dir/src/input/input.c.o.d"
+  "/home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/main.c" "CMakeFiles/TDC.dir/src/main.c.o" "gcc" "CMakeFiles/TDC.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TDC.dir/src/Myhill-Nerode/minimization.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/TDC.dir/src/Myhill-Nerode/minimization.c.o: \
+ /home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Myhill-Nerode/minimization.c \
+ /usr/include/stdc-predef.h \
+ /home/rodrigo/Documentos/GitHub/Teoria-da-Computacao/src/Myhill-Nerode/minimization.h
